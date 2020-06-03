9e118366dde6918eb1ee0687edec3d7bf346372b
palamalama <ericgemmell@gmail.com>
2020-06-03 17:27:51 +0100

    crude carousel working

--- src/components/eventCreator.js
+++ src/components/eventCreator.js
@@ -1,15 +1,41 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import Carousel, { Dots } from '@brainhubeu/react-carousel';
+import '@brainhubeu/react-carousel/lib/style.css';
 
 class EventCreator extends Component {
 	constructor() {
 		super();
-	}
+		this.state = {
+			mobile: true,
+			carouselIndex: 0,
+			slides: [
+				(<div>1</div>),
+				(<div>2</div>),
+				(<div>3</div>),
+			],
+			thumbnails: [
+				(<div>1</div>),
+				(<div>2</div>),
+				(<div>3</div>),
+			]
 
+		}
+		this.carouselChange = this.carouselChange.bind(this);
+		
+	}
+	carouselChange(carouselIndex){
+		this.setState({carouselIndex});
+	}
 	render() {
 		return (
-			<div>
-				event Creator
+			<div className={"EventCreator"}>
+				<Carousel centered infinite slidesPerPage={2}
+					value={this.state.value}
+					slides={this.state.slides}
+					onChange={this.carouselChange}
+				/>
+				<Dots number={this.state.thumbnails.length} thumbnails={this.state.thumbnails} value={this.state.value} onChange={this.carouselChange} number={this.state.slides.length} />
 			</div>
 		);
 	}
