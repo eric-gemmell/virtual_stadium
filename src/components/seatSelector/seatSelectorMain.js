a2dd5f3f1955e1e4959073b2160e752073e55d0a
palamalama <ericgemmell@gmail.com>
2020-06-16 12:19:10 +0100

    initial zooming version

--- src/components/seatSelector/seatSelectorMain.js
+++ src/components/seatSelector/seatSelectorMain.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "../images/logo.png";
 let d3 = null;
 let stadiumSvg = null;
 class SeatSelector extends Component {
@@ -19,12 +18,13 @@ class SeatSelector extends Component {
 	render(){
 		return(
 			<div>
-				{/*<div style={headerStyles(this.props.mobile)}>
-					<img src={logo} style={logoStyles(this.props.mobile)}/>
-					<div style={headerTextStyles(this.props.mobile)}>Seat Selector</div>
-				</div>*/}
 				<div id={"svg container"} style={{width:"100%",height:"100%"}} ref={(svgContainer) => this.svgContainer = svgContainer}>
-					
+					<svg style={{width:"100%"}} viewBox={"0 0 100 30"} >
+						<g id="zoom">
+							<rect className="selectable" x="10" y="10" width="10" height="10" style={{fill:"red",strokeWidth:"1",stroke:"black"}} />
+							<rect className="decoration" x="30" y="10" width="10" height="10" style={{fill:"blue",strokeWidth:"1",stroke:"black"}} />
+						</g>
+					</svg>
 				</div>	
 			</div>
 		      );
@@ -34,8 +34,8 @@ class SeatSelector extends Component {
 			console.log(stadiumSvg);
 			d3.xml(stadiumSvg).then(xml => {
 				console.log("d3 loaded and the image too, this is the data", xml);
-				let importedNode = document.importNode(xml.documentElement, true);
-				d3.select(this.svgContainer).node().appendChild(importedNode);
+				//let importedNode = document.importNode(xml.documentElement, true);
+				//d3.select(this.svgContainer).node().appendChild(importedNode);
 				d3.select(this.svgContainer)
 					.selectAll(".selectable")
 					.on("mouseover",function() {
@@ -46,40 +46,25 @@ class SeatSelector extends Component {
 					.on("mouseout",function(){
 						d3.select(this)
 							.style("stroke",d3.select(this).attr("oldStyle"));
+					})
+					.on("click", function(){
+						d3.select("svg")
+							.transition()
+							.duration(750)
+							.attr("viewBox","0 0 50 15");
 					});
+				console.log(d3.select(this.svgContainer).select(".selectable").node().getBBox());
+				d3.select("svg")
+					.call(d3.zoom().on("zoom", function () {
+						d3.select("#zoom").attr("transform", d3.event.transform)
+					}));
 			});
 			this.setState({svgDisplayed:true});
 		}
 	}	
+	zoomInOnSection(section){
+		
+	}
 }
 
 export default SeatSelector;
-let headerStyles = (mobile) => {
-	return {
-		backgroundColor:"#333",
-		paddingBottom: (mobile ? "2%": "0.5%"),
-		paddingTop: (mobile ?"2.5%":"1%"),
-		overflow:"hidden",
-		display: "flex",
-		alignItems: "center",
-	};
-};
-let logoStyles = (mobile) => {
-	return {
-		maxWidth:"20%",
-		maxHeight:(mobile)?"50px":"70px",
-		paddingLeft:"5%",
-	};
-};
-let headerTextStyles = (mobile) => {
-	return {
-		fontFamily: "Poppins, sans-serif",
-		textAlign: "center",
-		fontSize: (mobile)?"2em":"3em",
-		fontWeight: "600",
-		color: "white",
-		textShadow: "0 1px 2px rgba(255,255,255,.3)",
-		width:"80%",
-	};
-};
-
